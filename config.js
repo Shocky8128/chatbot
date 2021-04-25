e7bcf24c12e7590b17a435281ef6d33cb2f3253b
xvyn <71412865+Shocky8128@users.noreply.github.com>
2021-04-25 13:18:13 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //CHANGE ALL OF THESE
+    channelid: "834784434401050695",
+    token: "ODM1NzExMTcwNTU5ODY4OTQ5.YITarQ.BXXHiVbG9h4veeTOK0rxLA4UGUo",
+    botname: "Test Bot",
+
+    // API AUTHORIZATION
+    authorization: "NjMxMjI4MjM5NzYyOTQ4MTM3.MTYxODYxNDIzODczNw==.4b177c00d1fd218bfc890c91ecff0857"
+}
