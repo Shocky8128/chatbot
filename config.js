e991c203a4ec2b3709eb9a5f54024a6b4a2f1b07
xvyn <71412865+Shocky8128@users.noreply.github.com>
2021-04-25 13:20:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     //CHANGE ALL OF THESE
-    channelid: "834784434401050695",
-    token: "ODM1NzExMTcwNTU5ODY4OTQ5.YITarQ.BXXHiVbG9h4veeTOK0rxLA4UGUo",
-    botname: "Test Bot",
+    channelid: "Channel ID", //channel id you want me to chat in
+    token: "ur bot token", //ur bots token
+    botname: "bot name", //whatever name u want here
 
     // API AUTHORIZATION
-    authorization: "NjMxMjI4MjM5NzYyOTQ4MTM3.MTYxODYxNDIzODczNw==.4b177c00d1fd218bfc890c91ecff0857"
+    authorization: "api" //ur api authorization
 }
